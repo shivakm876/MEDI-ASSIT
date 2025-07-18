c0b44ab0ab767c9f4b30b033cb0925477658325a
shivakm876 <sivagangakm@gmail.com>
2025-07-18 17:50:19 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -12,3 +15,4 @@ const nextConfig = {
 }
 
 export default nextConfig
+
